da51b2e69c5670fb12b92e2d443e2a0a3e430190
John Overberg <john.richard.overberg@gmail.com>
2019-03-23 19:57:23 -0600

    stubbed structure & test controller works w/postman

--- DB/fueldb.sql
+++ DB/fueldb.sql
@@ -5,15 +5,15 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema fuel
+-- Schema fueldb
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `fuel` ;
+DROP SCHEMA IF EXISTS `fueldb` ;
 
 -- -----------------------------------------------------
--- Schema fuel
+-- Schema fueldb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `fuel` DEFAULT CHARACTER SET utf8 ;
-USE `fuel` ;
+CREATE SCHEMA IF NOT EXISTS `fueldb` DEFAULT CHARACTER SET utf8 ;
+USE `fueldb` ;
 
 -- -----------------------------------------------------
 -- Table `fillup`
@@ -44,7 +44,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- Data for table `fillup`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `fuel`;
+USE `fueldb`;
 INSERT INTO `fillup` (`id`, `time_stamp`, `odometer`, `price_per_gallon`, `gallons`) VALUES (1, '2019-03-01 19:25:00', 17500, 2.26, 8.3);
 INSERT INTO `fillup` (`id`, `time_stamp`, `odometer`, `price_per_gallon`, `gallons`) VALUES (2, '2019-03-03 20:30:00', 17850, 2.34, 8.6);
 INSERT INTO `fillup` (`id`, `time_stamp`, `odometer`, `price_per_gallon`, `gallons`) VALUES (3, '2019-03-06 08:34:00', 18206, 2.49, 9.0);
